8cfe6dbe8b709050f6cfea1626f3eec456abc936
Lucas Silva <lucasdemsilva2004@gmail.com>
2025-12-23 01:16:11 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+      document.getElementById("ano").textContent = new Date().getFullYear();
+      new Typed("#typed", {
+        strings: [
+          "Chocolate intenso.",
+          "Receitas artesanais.",
+          "Bolo de pote gourmet.",
+        ],
+        typeSpeed: 45,
+        backSpeed: 25,
+        loop: true,
+      });
